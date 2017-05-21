0e4561426e778e7959f5742d6689db09397c9a7e
MrCEO <allox03@hotmail.FR>
2017-05-21 02:38:42 +0200

    Move behavior into a function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-$( ".remover1" ).click(function() {
-	var a = confirm("Do you really want to remove this product?");
-	if (a) {
-		$( "ul" ).remove( ".product1" );
-	}
+/* use one method to perform a action */
+$( ".remover" ).click(function() {
+	deleteProduct(confirm("Do you really want to remove this product?"),$(this));
 });
-$( ".remover2" ).click(function() {
-	var a = confirm("Do you really want to remove this product?");
-	if (a) {
-		$( "ul" ).remove( ".product2" );
+
+function deleteProduct(toDelete,remover)
+{
+	if (toDelete) {
+		var idElementToRemove = remover.attr("id");
+		$( "#" + idElementToRemove.replace("romove-",'') ).fadeOut();
+		/*
+		$( "#" + idElementToRemove.replace("romove-",'') ).remove();
+		
+		i used fadeOut() because remove() delete the element without transition
+		*/
 	}
-});
-$( ".remover3" ).click(function() {
-	var a = confirm("Do you really want to remove this product?");
-	if (a) {
-		$( "ul" ).remove( ".product3" );
-	}
-});
+}
